114847d067e84cddd765752d66d02ecce25d3556
paras vekariya <113162042+Vekariyaparas@users.noreply.github.com>
2023-11-08 12:41:14 +0530

    Add files via upload

---
+++ JavaScript-Fundamentals/JS_Interactive_Quiz_Project/question.js
@@ -0,0 +1,112 @@
+let questions = [
+    {
+        numb: 1,
+        question: "what does HTML stand for?",
+        answer: "Hyper Text Markup Language",
+        options: [
+            "Hyper Text Mark Language",
+            "Hyper Text Markup Language",
+            "Hyper Texts Mark Language",
+            "Hyper Text multiple Language"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Java Script index always start with?",
+        answer: "0",
+        options: [
+            "0",
+            "1",
+            "2",
+            "3"
+        ]
+    },
+    {
+        numb: 3,
+        question: "What does CSS stand for?",
+        answer: "Cascading Style Sheet",
+        options: [
+            "Common Style Sheet",
+            "Colorful Style Sheet",
+            "Computer Style Sheet",
+            "Cascading Style Sheet"
+        ]
+    },
+    {
+        numb: 4,
+        question: "FAG, GAF, HAI, IAH, ____",
+        answer: "JAK",
+        options: [
+            "JAI",
+            "HAK",
+            "JAK",
+            "HAL"
+        ]
+    },
+    {
+        numb: 5,
+        question: "Which word does NOT beloing with the others?",
+        answer: "ounce",
+        options: [
+            "inch",
+            "ounce",
+            "yard",
+            "centimeter"
+        ]
+    },
+    {
+        numb: 6,
+        question: "Which word does NOT beloing with the others?",
+        answer: "car",
+        options: [
+            "tyre",
+            "car",
+            "engine",
+            "steering wheel"
+        ]
+    },
+    {
+        numb: 7,
+        question: "CMM, EOO, GQQ, ____, KUU",
+        answer: "ISS",
+        options: [
+            "GRR",
+            "GSS",
+            "ISS",
+            "ITT"
+        ]
+    },
+    {
+        numb: 8,
+        question: "what is NON PRIMITIVE data type?",
+        answer: "Array",
+        options: [
+            "String",
+            "Number",
+            "Array",
+            "BigInt"
+        ]
+    },
+    {
+        numb: 9,
+        question: "Complete the series 2, 5, 9, 19, 37.......",
+        answer: "75",
+        options: [
+            "76",
+            "74",
+            "75",
+            "None of This"
+        ]
+    },
+    {
+        numb: 10,
+        question: "What is the average of first five multiples of 12?",
+        answer: "36",
+        options: [
+            "38",
+            "40",
+            "42",
+            "36"
+        ]
+    }
+];
